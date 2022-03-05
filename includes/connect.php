fad8f4716cf997d3be43a021e84a72956d31a066
Renee <97077802+RHolter@users.noreply.github.com>
2022-03-05 15:23:57 -0800

    Delete includes directory

--- includes/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-// connect to database
-$username = "rholterg_grcuser";
-$password = "S3bastian!";
-$hostname = "localhost";
-$database = "rholterg_grc";
-
-$cnxn = mysqli_connect($hostname, $username, $password, $database)
-or die("<p>Oops! we weren't able to connect to the database.</p>");
